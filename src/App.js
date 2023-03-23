93197d6207a7f01b9b8e00b835f5bb22ee124c95
jazeel659 <jaseel0659@gmail.com>
2023-03-23 15:48:49 +0530

    dete and complete button added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import List from "./components/List";
 import { useState } from "react";
 function App() {
   const [data, setdata] = useState([]);
+  const [filter, setfilter] = useState(["ALL", "PENDING"]);
+  const [selected, setselected] = useState("ALL");
   const addTask = (todo) => {
-    const newTask = [...data, todo];
+    const newTask = [...data, { task: todo, iscompleted: false }];
     setdata(newTask);
   };
   const deleteTask = (index) => {
@@ -15,14 +17,52 @@ function App() {
     setdata(newData);
     console.log(index);
   };
+  const completedTask = (index) => {
+    const updated = [...data];
+    updated[index].iscompleted = true;
+    console.log(updated);
+    setdata(updated);
+  };
   return (
     <div className="main">
       <h1>todo app</h1>
       <Input addTask={addTask}></Input>
+      <h4>TODO LIST</h4>
+      {data.length > 0 ? (
+        <div className="filter-div">
+          {filter.map((x) => {
+            return (
+              <div
+                style={{ color: selected === x ? "green" : "red" }}
+                onClick={() => {
+                  setselected(x);
+                }}
+              >
+                {x}
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div style={{ color: "red" }}>no task</div>
+      )}
+
       <div className="list-div">
-        {data.map((x, index) => {
-          return <List data={x} index={index} deleteTask={deleteTask}></List>;
-        })}
+        {data
+          .filter((y) => {
+            return selected === "ALL" ? y : y.iscompleted === false;
+          })
+          .map((x, index) => {
+            console.log(x);
+            return (
+              <List
+                data={x.task.toString()}
+                index={index}
+                deleteTask={deleteTask}
+                completedTask={completedTask}
+              ></List>
+            );
+          })}
       </div>
     </div>
   );
--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,27 @@
 import React from "react";
 import "../App.css";
 
-function List({ data, index, deleteTask }) {
+function List({ data, index, deleteTask, completedTask }) {
   return (
     <div className="list-container">
+      {console.log(typeof data)}
       {data}
-      <button
-        onClick={() => {
-          deleteTask(index);
-        }}
-      >
-        delete
-      </button>
+      <div className="button-div">
+        <button
+          onClick={() => {
+            completedTask(index);
+          }}
+        >
+          completed
+        </button>
+        <button
+          onClick={() => {
+            deleteTask(index);
+          }}
+        >
+          delete
+        </button>
+      </div>
     </div>
   );
 }
